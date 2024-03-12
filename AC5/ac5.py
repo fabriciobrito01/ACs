4e8ee66f65d9d915c9cbccd036c8037f4d55c56b
Fabrício de Brito <132503568+fabriciobrito01@users.noreply.github.com>
2024-03-12 16:56:35 -0300

    Update ac5.py

--- AC5/ac5.py
+++ AC5/ac5.py
@@ -11,6 +11,7 @@ import random
 
 
 def main():
+
     aventureiro_vida = 100
     aventureiro_atk = random.randint(10, 20)
     aventureiro_def = random.randint(1, 5)
@@ -25,7 +26,10 @@ def main():
     while aventureiro_vida > 0 and monstro_vida > 0:
         print('Rodada', rodada)
         monstro_vida = monstro_vida - random.randint(1, aventureiro_atk)
-        aventureiro_vida = aventureiro_vida - random.randint(1, monstro_atk - aventureiro_def)
+        dano_monstro = random.randint(1, monstro_atk) - aventureiro_def
+
+        if dano_monstro > 0:
+            aventureiro_vida = aventureiro_vida - dano_monstro
 
         if monstro_vida <= 0:
             print('O montro morreu!')
