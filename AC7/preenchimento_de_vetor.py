833837404416ac9e747035d01c222b38614855e3
Fabrício de Brito <fabriciobef@hotmail.com>
2024-04-21 19:18:21 -0300

    ac7 ex4 finalizado

---
+++ AC7/preenchimento_de_vetor.py
@@ -0,0 +1,18 @@
+'''
+Programação Estruturada
+2024.1
+21/04/2024
+AC7
+Prof: Victor Machado
+Aluno: Fabrício de Brito
+'''
+
+v = int(input())
+
+if v <= 50: 
+    i = 0
+    while i < 10:
+        x = v
+        v = v * 2
+        print(f'N[{i}] = {x}')
+        i += 1
